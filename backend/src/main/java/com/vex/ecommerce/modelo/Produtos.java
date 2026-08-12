648c41a6b404a5518eb8d86e0198a08441e108b0
Eduardo Duran <eduardoduranteodoro4@gmail.com>
2026-08-11 22:10:59 -0300

    feat: add Produtos class

---
+++ backend/src/main/java/com/vex/ecommerce/modelo/Produtos.java
@@ -0,0 +1,9 @@
+//* Esta classe representa os produtos disponíveis no e-commerce. Ela contém informações sobre cada produto, como nome, descrição, preço e quantidade em estoque. A classe também pode incluir métodos para manipular os produtos, como adicionar novos produtos, atualizar informações existentes e remover produtos do catálogo.
+
+package com.vex.ecommerce.modelo;
+
+public class Produtos {
+    public static void main(String[] args) {
+        
+    }
+}
