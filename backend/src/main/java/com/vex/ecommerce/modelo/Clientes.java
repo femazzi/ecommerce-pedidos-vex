5cec076dd9bd9d42d17c0309acb2baa76d50cfc3
Felipe Mazzi <felipe.mazzi@hotmail.com>
2026-08-11 21:58:10 -0300

    feat: create Clientes model class for client management

---
+++ backend/src/main/java/com/vex/ecommerce/modelo/Clientes.java
@@ -0,0 +1,7 @@
+// * Essa classe será responsável por criar o modelo de clientes, com seus atributos e métodos.
+// * Cuidando do cadastro, atualização e exclusão de clientes no sistema.
+public class Clientes {
+    public static void main(String[] args) {
+        
+    }
+}
